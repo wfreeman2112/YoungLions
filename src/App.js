1e9fa2d9b8926f2394d3d11fa2468fd1d1e587fe
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-10 10:32:18 -0600

    creating functions to display data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Welcome from './components/Welcome';
 import './App.css';
+import Welcome from './components/Welcome';
+import { wrestlerData } from './Data/wrestlerData'
+import wrestlerList from './components/wrestlerList'
 
 function App() {
   return (
     <div className="container">
       <Welcome />
-
+      <wrestlerList wrestlers={wrestlerData} />
     </div>
   );
 }
---
+++ src/components/wrestlerList.js
@@ -0,0 +1,23 @@
+import Wrestlers from './wrestlers'
+import '../App.css';
+
+function wrestlerList(props){
+    const allWrestlers = props.wrestlers.map(wrestlers => {
+        return (
+            <Wrestlers
+            image={wrestlers.img}
+            name={wrestlers.name}
+            birthday={wrestlers.birthday}
+            finisher={wrestlers.finisher}
+            />
+        )
+    })
+
+    return (
+        <div className="flex-container">
+            {allWrestlers}
+        </div>
+    )
+}
+
+export default wrestlerList
